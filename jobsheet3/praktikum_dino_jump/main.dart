2cdbbfe6ae53b191dbcfa5932db777a03ab3df28
Ericharw <ericharizki12@gmail.com>
2025-09-16 21:28:08 +0700

    Modifikasi praktikum

--- jobsheet3/praktikum_dino_jump/main.dart
+++ jobsheet3/praktikum_dino_jump/main.dart
@@ -7,9 +7,11 @@ void main() {
   var dino = Dino(0, 0);
   var cactus = Cactus(10, 0);
   // Simulate game loop
-  List<GameObject> gameObjects = [dino, cactus];
-  for (var obj in gameObjects) {
+  List<GameObject> GameObjects = [dino, cactus];
+  for (var obj in GameObjects) {
     obj.update();
+  }
+  for (var obj in GameObjects) {
     obj.render();
   }
   // Trigger specific behavior
